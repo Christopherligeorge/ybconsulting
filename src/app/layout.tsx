224e459b8f424b14aaef06561ba1df0a4937ee2e
RADBRAD1 <112186951+RADBRAD1@users.noreply.github.com>
2024-07-03 19:54:20 -0400

    v1.0000003

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,49 @@
+import Navbar from '@/components/Navbar'
+import Providers from '@/components/Providers'
+import { cn, constructMetadata } from '@/lib/utils'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+import 'react-loading-skeleton/dist/skeleton.css'
+import 'simplebar-react/dist/simplebar.min.css'
+
+import { Toaster } from '@/components/ui/toaster'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = constructMetadata()
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang='en' className='light'>
+      <Providers>
+        <body
+          className={cn(
+            'min-h-screen font-sans antialiased grainy',
+            inter.className
+          )}>
+          <Toaster />
+          <Navbar />
+          {children}
+        </body>
+      </Providers>
+    </html>
+  )
+}
+
+
+
+
+//Make changes to home page layout here in layout.tsx (such as color of background)
+/*
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,3 +62,4 @@ export default function RootLayout({
     </html>
   );
 }
+*/
---
+++ src/components/ui/toaster.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+//use client defines the boundary between server and client code on the module dependency
+// tree.  In the module dependency tree, we see that App.
+
+import {
+  Toast,
+  ToastClose,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from "@/components/ui/toast"
+import { useToast } from "@/components/ui/use-toast"
+
+export function Toaster() {
+  const { toasts } = useToast()
+
+  return (
+    <ToastProvider>
+      {toasts.map(function ({ id, title, description, action, ...props }) {
+        return (
+          <Toast key={id} {...props}>
+            <div className="grid gap-1">
+              {title && <ToastTitle>{title}</ToastTitle>}
+              {description && (
+                <ToastDescription>{description}</ToastDescription>
+              )}
+            </div>
+            {action}
+            <ToastClose />
+          </Toast>
+        )
+      })}
+      <ToastViewport />
+    </ToastProvider>
+  )
+}
