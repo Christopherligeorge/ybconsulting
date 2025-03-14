6a6f5d68ccf3bec5b5cae6018255b9c8975f3546
RADBRAD1 <112186951+RADBRAD1@users.noreply.github.com>
2025-03-14 18:02:22 -0400

    3/14/25-v7

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -5,23 +5,7 @@
 
 //in nextJS,  the a request is always of type NextRequest, (change if there is a implicity of type 'any' error)
 
-
-
 import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
-import { NextRequest } from 'next/server'
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { kindeAuth: string } }
-) {
-  const authHandler = await handleAuth()
-  return authHandler(req, params.kindeAuth)
-}
-
-export async function POST(
-  req: NextRequest,
-  { params }: { params: { kindeAuth: string } }
-) {
-  const authHandler = await handleAuth()
-  return authHandler(req, params.kindeAuth)
-}
+export const GET = handleAuth()
+export const POST = handleAuth()
