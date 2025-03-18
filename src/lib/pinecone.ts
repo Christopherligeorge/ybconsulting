2768d603eb1186ae9d5f7d198fb80d91f9c218fe
RADBRAD1 <112186951+RADBRAD1@users.noreply.github.com>
2025-03-18 10:13:23 -0400

    3/18/25-v6

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,12 +1,8 @@
-import  {PineconeClient} from '@pinecone-database/pinecone'
+import { Pinecone } from '@pinecone-database/pinecone'
 
 export const getPineconeClient = async () => {
-  const client = new PineconeClient()
-
-  await client.init({
-    apiKey: process.env.PINECONE_API_KEY!,
-    environment: 'aped-4627-b74a.',
+  const client = new Pinecone({
+    apiKey: process.env.PINECONE_API_KEY!
   })
-
   return client
 }
