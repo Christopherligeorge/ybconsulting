1000ded26e0dfd3cc3022284d299a1d520ca2423
RADBRAD1 <112186951+RADBRAD1@users.noreply.github.com>
2025-03-18 10:24:35 -0400

    3/18/25-v7

--- next.config.mjs
+++ next.config.mjs
@@ -2,33 +2,43 @@
 
 //need custom webpack in nextConfig to render pdfs(b/c cant render like images.)
 const nextConfig = {
-  
+  output: 'standalone',
+  experimental: {
+    serverActions: true,
+  },
   images: {
     domains: ['gravatar.com'],
   },
-    async redirects() {
-      return [
-        {
-          source: '/sign-in',
-          destination: '/api/auth/login',
-          permanent: true,
-        },
-        {
-          source: '/sign-up',
-          destination: '/api/auth/register',
-          permanent: true,
-        },
-      ];
-    },
-  
-    webpack: (
-      config,
-      { buildId, dev, isServer, defaultLoaders, webpack }
-    ) => {
-      config.resolve.alias.canvas = false;
-      config.resolve.alias.encoding = false;
-      return config;
-    },
-  };
-  
-  export default nextConfig;
+  async redirects() {
+    return [
+      {
+        source: '/sign-in',
+        destination: '/api/auth/login',
+        permanent: true,
+      },
+      {
+        source: '/sign-up',
+        destination: '/api/auth/register',
+        permanent: true,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ]
+  },
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, webpack }
+  ) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
+};
+
+export default nextConfig;
