381aad42267b9de7fbb8478d1b16896093739944
RADBRAD1 <112186951+RADBRAD1@users.noreply.github.com>
2025-03-18 10:52:22 -0400

    3/18/25-v8

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  env: {
+    KINDE_CLIENT_ID: process.env.KINDE_CLIENT_ID,
+    KINDE_CLIENT_SECRET: process.env.KINDE_CLIENT_SECRET,
+    KINDE_ISSUER_URL: process.env.KINDE_ISSUER_URL,
+    KINDE_SITE_URL: process.env.KINDE_SITE_URL,
+    KINDE_POST_LOGOUT_REDIRECT_URL: process.env.KINDE_POST_LOGOUT_REDIRECT_URL,
+    KINDE_POST_LOGIN_REDIRECT_URL: process.env.KINDE_POST_LOGIN_REDIRECT_URL,
+  },
   images: {
     domains: ['gravatar.com'],
   },
